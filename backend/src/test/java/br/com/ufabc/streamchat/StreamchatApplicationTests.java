55f6775e851edb20fd3cd248ff4e7507efc03730
matheus <matheus0210@hotmail.com>
2023-03-26 13:44:48 -0300

    feat: backend setup

---
+++ backend/src/test/java/br/com/ufabc/streamchat/StreamchatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufabc.streamchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
