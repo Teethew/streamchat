9c77522c1b0fbe2ce8dd16b1ea46e19ea9dba49c
Renan Zago Lorijola <renanzago@gmail.com>
2023-04-13 03:07:20 -0300

    try multiple calls

--- frontend/src/components/videoCall.tsx
+++ frontend/src/components/videoCall.tsx
@@ -1,96 +1,121 @@
 "use client";
 
-import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import {
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  Fragment,
+  useState,
+} from "react";
 
 import { Peer } from "peerjs";
+import axios from "axios";
 
 type VideoCallProps = {
   id: string;
 };
 
 const VideoCall = ({ id, children }: PropsWithChildren<VideoCallProps>) => {
-  const localAudioRef = useRef<HTMLVideoElement>(null);
-  const remoteAudioRef = useRef<HTMLVideoElement>(null);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideosRef = useRef<HTMLDivElement>(null);
 
-  const peer = useRef(
-    new Peer(`streamchat-${id}-${localStorage.getItem("username")}`)
-  );
+  const [streams, setStreams] = useState<MediaStream[]>([]);
 
   useEffect(() => {
+    if (remoteVideosRef.current) {
+      Array.from(remoteVideosRef.current.querySelectorAll("video")).forEach(
+        (video, index) => {
+          if (video.srcObject) return;
+          video.srcObject = streams[index - 1];
+        }
+      );
+    }
+  }, [streams]);
+
+  useEffect(() => {
+    const peer = new Peer(
+      `streamchat-${id}-${localStorage.getItem("username")}`
+    );
     if (window) {
-      peer.current.on("call", (call) => {
+      peer.on("call", (call) => {
         navigator.mediaDevices
           .getUserMedia({ video: true, audio: true })
           .then((stream) => {
-            localAudioRef.current!.srcObject = stream;
+            localVideoRef.current!.srcObject = stream;
             call.answer(stream);
+            console.log("executou fora do on stream on call");
             call.on("stream", (remoteStream) => {
-              remoteAudioRef.current!.srcObject = remoteStream;
+              if (!remoteVideosRef.current) return;
+              console.log("executou on call");
+              setStreams((prevStreams) => [
+                ...prevStreams.filter((it) => it.id !== remoteStream.id),
+                remoteStream,
+              ]);
             });
           })
           .catch((err) => {
             console.error("Failed to get local stream", err);
           });
       });
-    }
-  }, []);
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          localVideoRef.current!.srcObject = stream;
+          axios
+            .get<{ id: number; usuario: string; sala: string }[]>(
+              `https://streamchat-production.up.railway.app/current/${id}`
+            )
+            .then(({ data }) => {
+              const otherUsers = data
+                .map((it) => it.usuario)
+                .filter((it) => it !== localStorage.getItem("username"));
 
-  const handleCallPerson = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        localAudioRef.current!.srcObject = stream;
-        const call = peer.current.call(`streamchat-${id}-renan`, stream);
-        call.on("stream", (remoteStream) => {
-          remoteAudioRef.current!.srcObject = remoteStream;
+              console.log(otherUsers);
+
+              for (const user of otherUsers) {
+                const call = peer.call(`streamchat-${id}-${user}`, stream);
+                console.log("executou fora do on stream do call");
+                call.on("stream", (remoteStream) => {
+                  if (!remoteVideosRef.current) return;
+                  console.log("executou do call");
+                  setStreams((prevStreams) => [
+                    ...prevStreams.filter((it) => it.id !== remoteStream.id),
+                    remoteStream,
+                  ]);
+                });
+              }
+            });
+        })
+        .catch((err) => {
+          console.error("Failed to get local stream", err);
         });
-      })
-      .catch((err) => {
-        console.error("Failed to get local stream", err);
-      });
-  };
+    }
+    return () => {
+      peer.destroy();
+    };
+  }, []);
 
   return (
     <>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-5" ref={remoteVideosRef}>
         <video
-          ref={localAudioRef}
+          ref={localVideoRef}
           className="border border-white"
           playsInline
           autoPlay
           muted
         />
-        <video
-          ref={remoteAudioRef}
-          className="border border-white"
-          playsInline
-          autoPlay
-        />
+        {streams.map((_stream, index) => (
+          <video
+            key={index}
+            className="border border-white"
+            playsInline
+            autoPlay
+          />
+        ))}
       </div>
       {children}
-      <div className="flex">
-        <button
-          className="flex justify-center items-center hover:bg-gradient-to-br focus:ring-1 focus:ring-secondary bg-gradient-to-tr from-primary to-secondary w-10 h-10 rounded-full cursor-pointer"
-          onClick={handleCallPerson}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            strokeWidth="1"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-            <path d="M15 7a2 2 0 0 1 2 2"></path>
-            <path d="M15 3a6 6 0 0 1 6 6"></path>
-          </svg>
-        </button>
-      </div>
+      <div className="flex"></div>
     </>
   );
 };
